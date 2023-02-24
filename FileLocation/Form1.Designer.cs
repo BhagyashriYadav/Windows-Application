4300cfb8332567e4d2071f7a6c1f4e8696a83888
Miss. Bhagyashri Yadav <115793640+BhagyashriYadav@users.noreply.github.com>
2023-02-24 17:46:45 +0530

    Add files via upload

---
+++ FileLocation/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace File_Location
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.open = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.path.Location = new System.Drawing.Point(99, 56);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(76, 16);
+            this.path.TabIndex = 0;
+            this.path.Text = "File Path:";
+            this.path.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // open
+            // 
+            this.open.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open.Location = new System.Drawing.Point(149, 125);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(123, 35);
+            this.open.TabIndex = 2;
+            this.open.Text = "Open File";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.Location = new System.Drawing.Point(358, 125);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(123, 35);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Delete File";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // status
+            // 
+            this.status.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(260, 202);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(123, 35);
+            this.status.TabIndex = 4;
+            this.status.Text = "Check Status";
+            this.status.UseVisualStyleBackColor = true;
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(674, 318);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label path;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button status;
+    }
+}
+
---
+++ FileLocation/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace File_Location
+{
+    public partial class Form1 : Form
+    {
+        Process file = new Process();
+       
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog = new OpenFileDialog();
+            if(OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = Process.Start(OpenFileDialog.FileName);
+                textBox1.Text = OpenFileDialog.FileName;
+            }
+
+
+            //show only txt file
+            // OpenFileDialog OpenFileDialog = new OpenFileDialog();
+            //if(OpenFileDialog.ShowDialog()==DialogResult.OK)
+            // {
+            //   String text = File.ReadAllText(OpenFileDialog.FileName);
+            //   richTextBox1.Text = text;
+            //  textBox1.Text = OpenFileDialog.FileName;
+
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd = new OpenFileDialog();
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+
+                textBox1.Text = fbd.FileName;
+            }
+        }
+        
+        
+        private void status_Click(object sender, EventArgs e)
+        {
+           
+
+            string filepath = textBox1.Text;
+           
+            try
+            {
+                using (Stream stream = new FileStream(textBox1.Text, FileMode.Open))
+                {
+
+                    MessageBox.Show("file is open");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("file not open");
+            }
+
+
+            
+            //string filepath = textBox1.Text;
+            //if(File.Exists(filepath))
+            //{
+            //    MessageBox.Show("file exist");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("not exist");
+            //}
+        }
+
+       
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+
+           
+            string filepath = textBox1.Text;
+            if(File.Exists(filepath))
+            {
+                File.Delete(filepath);
+                MessageBox.Show("file deleted");
+            }
+            else
+            {
+               
+            }
+            textBox1.Clear();
+        }
+    }
+    
+}
